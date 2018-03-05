e834016a8c4ee12af00c2f8224d3c96b54ed34f9
Adriano Oliveira <drprado2@gmail.com>
2018-03-05 02:42:16 -0300

    Criado serviço aspnetcore.

---
+++ app-api/MinhaEmpresa.Dominio.Api/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaEmpresa.Dominio.Api.Controllers
+{
+    [Route("/api/customer")]
+    public class CustomerController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Está funcionando a API");
+        }
+    }
+}
